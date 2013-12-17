84ebb846607e6b27659bc46b6dac90dd60bbd5c9
Sebastian Henschel <sh@sinnwerkstatt.com>
2013-12-17 12:55:10 +0100

    Added translatable COSINNUS_APP_LABEL

--- cosinnus_event/cosinnus_app.py
+++ cosinnus_event/cosinnus_app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
 
 IS_COSINNUS_APP = True
 COSINNUS_APP_NAME = 'event'
-COSINNUS_APP_LABEL = 'Events'
-
+COSINNUS_APP_LABEL = _('Event')
