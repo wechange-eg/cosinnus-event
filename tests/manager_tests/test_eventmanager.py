1c1958214e5650dd86d8b88c06e48cf3d2c7e26f
Sebastian Henschel <sh@sinnwerkstatt.com>
2013-12-18 17:42:54 +0100

    Added manager tests for upcoming events

--- tests/manager_tests/test_eventmanager.py
+++ tests/manager_tests/test_eventmanager.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import timedelta
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils.timezone import now
 
 from cosinnus.models import CosinnusGroup
 from cosinnus_event.models import Event
@@ -52,3 +54,44 @@ class EventManagerTest(TestCase):
         self.event.public = True
         self.event.save()
         self.assertListEqual([], list(Event.objects.public()))
+
+    def test_upcoming_in_past(self):
+        """
+        Should have no upcoming events if event is in past
+        """
+        self.event.from_date = now() - timedelta(days=2)
+        self.event.to_date = now() - timedelta(days=1)
+        self.event.save()
+        self.assertListEqual([], list(Event.objects.upcoming(count=1)))
+
+    def test_upcoming_in_future(self):
+        """
+        Should have upcoming events if event is in future
+        """
+        self.event.from_date = now() + timedelta(days=1)
+        self.event.to_date = now() + timedelta(days=2)
+        self.event.save()
+        self.assertEqual(self.event, Event.objects.upcoming(count=1)[0])
+
+    def test_upcoming_count_in_future(self):
+        """
+        Should have upcoming count events if event is in future
+        """
+        count = 3
+        for i in xrange(1, count + 2):  # + 2 to get count+1 upcoming events
+            Event.objects.create(
+                group=self.group,
+                created_by=self.admin,
+                public=True,
+                title='testevent %d' % i,
+                state=Event.STATE_SCHEDULED,
+                from_date = now(),
+                to_date = now() + timedelta(days=1))
+
+        num_events = len(Event.objects.all())
+        self.assertLess(count, num_events)
+
+        num_upcoming_all = len(Event.objects.upcoming(count=num_events))
+        num_upcoming_count = len(Event.objects.upcoming(count=count))
+        self.assertLess(num_upcoming_count, num_upcoming_all)
+
