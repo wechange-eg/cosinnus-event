997813e24f9c1e2245a56c082a051ac35297deec
Sebastian Henschel <sh@sinnwerkstatt.com>
2013-12-22 15:50:55 +0100

    Refers #5 -- Removed local conf's DATETIME_PICK_FORMAT in favour of cosinnus.formats

--- cosinnus_event/conf.py
+++ cosinnus_event/conf.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
+from django.conf import settings  # noqa
 from appconf import AppConf
 
 
 class CosinnusEventConf(AppConf):
-    DATETIME_PICK_FORMAT = getattr(
-        settings, 'COSINNUS_DATETIME_PICK_FORMAT', None) or 'YYYY-MM-DD HH:mm'
+    pass
