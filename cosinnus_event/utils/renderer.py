44938f3e6472404a7f60d0ee436e59f0f28eb8d8
Sascha Narr <saschanarr@googlemail.com>
2014-08-13 17:56:30 +0200

    Basic single_object template and renderer setting

--- cosinnus_event/utils/renderer.py
+++ cosinnus_event/utils/renderer.py
@@ -9,7 +9,8 @@ class EventRenderer(BaseRenderer):
     EventRenderer for Cosinnus attached objects
     """
     template = 'cosinnus_event/attached_events.html'
-
+    template_single = 'cosinnus_event/single_event.html'
+    
     @classmethod
     def render(cls, context, myobjs):
         return super(EventRenderer, cls).render(context, events=myobjs)
