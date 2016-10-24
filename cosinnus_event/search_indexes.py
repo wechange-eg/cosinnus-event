4ed8b7910a7d663b4009e3e017d8fae16507e996
Sascha Narr <saschanarr@googlemail.com>
2016-10-24 18:08:09 +0200

    Refactor search index text

--- cosinnus_event/search_indexes.py
+++ cosinnus_event/search_indexes.py
@@ -9,14 +9,6 @@ from cosinnus_event.models import Event
 
 
 class EventIndex(BaseTaggableObjectIndex, indexes.Indexable):
-    note = indexes.CharField(model_attr='note', null=True)
-    location = indexes.CharField(model_attr='location', null=True)
-    street = indexes.CharField(model_attr='street', null=True)
-    zipcode = indexes.CharField(model_attr='zipcode', null=True)
-    city = indexes.CharField(model_attr='city', null=True)
-
-    image = indexes.CharField(model_attr='image', indexed=False, null=True)
-    url = indexes.CharField(model_attr='url', indexed=False, null=True)
 
     def get_model(self):
         return Event
