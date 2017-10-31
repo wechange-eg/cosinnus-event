8c4ea582b67efc4b3c6c841d013f6d38a3875b67
Sascha Narr <saschanarr@googlemail.com>
2017-10-31 16:18:18 +0100

    Map haystack endpoint wip

--- cosinnus_event/search_indexes.py
+++ cosinnus_event/search_indexes.py
@@ -9,7 +9,14 @@ from cosinnus_event.models import Event
 
 
 class EventIndex(BaseTaggableObjectIndex, indexes.Indexable):
-
+    
+    from_date = indexes.DateTimeField(model_attr='from_date', null=True)
+    to_date = indexes.DateTimeField(model_attr='to_date', null=True)
+    needs_date_filtering = indexes.BooleanField()
+    
+    def prepare_needs_date_filtering(self, obj):
+        return True
+    
     def get_model(self):
         return Event
 
