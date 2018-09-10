d12f06c802c3fe0f5e0568c2a9606d7381fc2ed5
Sascha Narr <saschanarr@googlemail.com>
2018-09-10 14:24:45 +0200

    cooperation #122: New ranking for non-query search results: Events/Groups/Projects/Ideas use a hybrid interestingness/popularity ranking, people are dampened.

--- cosinnus_event/search_indexes.py
+++ cosinnus_event/search_indexes.py
@@ -5,7 +5,10 @@ from haystack import indexes
 
 from cosinnus.utils.search import BaseTaggableObjectIndex, StoredDataIndexMixin
 
-from cosinnus_event.models import Event, EventAttendance
+from cosinnus_event.models import Event, EventAttendance,\
+    annotate_attendants_count
+from cosinnus.utils.functions import normalize_within_stddev
+from django.utils.timezone import now
 
 
 class EventIndex(BaseTaggableObjectIndex, StoredDataIndexMixin, indexes.Indexable):
@@ -37,5 +40,35 @@ class EventIndex(BaseTaggableObjectIndex, StoredDataIndexMixin, indexes.Indexabl
     def prepare_humanized_event_time_html(self, obj):
         return obj.get_humanized_event_time_html()
     
+    def boost_model(self, obj, indexed_data):
+        """ We boost a combined measure of 2 added factors: soonishnes (50%) and participant count (50%).
+            This means that a soon happening event with lots of participants will rank highest and an far off event 
+            with no participants lowest.
+            But it also means that soon happening events with no participants will still rank quite high, 
+            as will far off events with lots of participants.
+            
+            Factors:
+            - 50%: the event's participant count, normalized over the mean/stddev of the participant count of all 
+                other events, in a range of [0.0..1.0]
+            - 50%: the event's date, highest being now() and lowest >= 12 months from now
+            """
+        
+        def qs_func():
+            return annotate_attendants_count(Event.get_current_for_portal())
+        
+        mean, stddev = self.get_mean_and_stddev(qs_func, 'attendants_count', non_annotated_property=True)
+        current_participant_count = self.prepare_participant_count(obj)
+        rank_from_participants = normalize_within_stddev(current_participant_count, mean, stddev, stddev_factor=1.0)
+        
+        if obj.from_date:
+            future_date_timedelta = obj.from_date - now()
+            if future_date_timedelta.days < 0:
+                rank_from_date = 0.0  # past events rank worst 
+            else:
+                rank_from_date = max(1.0 - (future_date_timedelta.days/365.0), 0) 
+        else:
+            rank_from_date = 0.0
+            
+        return (rank_from_participants / 2.0) + (rank_from_date / 2.0)
     
     
