3393822d63fba96a1d1210025fb05e3ec1e4fbf1
sascha <sn@sinnwerkstatt.com>
2018-11-13 16:09:49 +0100

    Django 2.0 compatibility

--- cosinnus_event/urls.py
+++ cosinnus_event/urls.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 from cosinnus_event import views
 
+app_name = 'event'
+
 cosinnus_group_patterns = [
     url(r'^$', views.index_view, name='index'),
     url(r'^calendar/$', views.list_view, name='list'),
