acb430b12993227c4dbd77ad05d3fb7e520a8081
sascha <sn@sinnwerkstatt.com>
2018-11-18 19:22:07 +0100

    Updated filters for django-filters==2.0.0

--- cosinnus_event/filters.py
+++ cosinnus_event/filters.py
@@ -6,23 +6,32 @@ Created on 05.08.2014
 from builtins import object
 from django.utils.translation import ugettext_lazy as _
 
-from cosinnus.views.mixins.filters import CosinnusFilterSet
-from cosinnus.forms.filters import AllObjectsFilter, SelectCreatorWidget
+from cosinnus.views.mixins.filters import CosinnusFilterSet,\
+    CosinnusOrderingFilter
+from cosinnus.forms.filters import AllObjectsFilter, SelectCreatorWidget,\
+    DropdownChoiceWidget
 from cosinnus_event.models import Event
 
 
 class EventFilter(CosinnusFilterSet):
     creator = AllObjectsFilter(label=_('Created By'), widget=SelectCreatorWidget)
     
-    class Meta(object):
-        model = Event
-        fields = ['creator']
-        order_by = (
+    o = CosinnusOrderingFilter(
+        fields=(
+            ('from_date', 'from_date'),
+            ('created', 'created'),
+            ('title', 'title'),
+        ),
+        choices=(
             ('from_date', _('Soonest Upcoming')),
             ('-created', _('Newest Created')),
             ('title', _('Title')),
-        )
+        ),
+        default='from_date',
+        widget=DropdownChoiceWidget
+    )
     
-    def get_order_by(self, order_value):
-        return super(EventFilter, self).get_order_by(order_value)
+    class Meta(object):
+        model = Event
+        fields = ['creator', 'o']
     
