1035cc8cca5c064f8c0f787fe6d45b6d7690c5a8
Simon Liedtke <simon.liedtke@gmail.com>
2021-03-19 19:41:44 +0100

    Merge branch 'staging' into feature/user-api
    # Conflicts:
    #	cosinnus/api/views/__init__.py
    
    Merge branch 'staging' into feature/user-api
    
    # Conflicts:
    #	cosinnus_event/api/views.py

--- cosinnus_event/api/views.py
+++ cosinnus_event/api/views.py
@@ -2,8 +2,7 @@
 from django.db.models import Q
 from rest_framework import viewsets
 
-from cosinnus.api.views.mixins import PublicTaggableObjectFilterMixin, CosinnusFilterQuerySetMixin
-from cosinnus.api.views import PublicTaggableObjectFilterMixin, CosinnusFilterQuerySetMixin,\
+from cosinnus.api.views.mixins import PublicTaggableObjectFilterMixin, CosinnusFilterQuerySetMixin, \
     CosinnusPaginateMixin
 from cosinnus_event.models import Event
 from cosinnus_event.api.serializers import EventListSerializer, EventRetrieveSerializer
