494f4a121610615381b3d293a14f8ce2c80e78f1
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-06-18 13:39:18 +0200

    Add new start and endtimes to urls as params

--- cosinnus_event/views.py
+++ cosinnus_event/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from builtins import str
 from collections import defaultdict
 import dateutil.parser
+from datetime import timedelta
 
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied
@@ -1062,12 +1063,23 @@ class ConferenceEventFormMixin(RequireWriteMixin, FilterGroupMixin, FilterConfer
 
     def get_success_url(self):
         if 'create_another' in self.request.POST:
-            to_date = self.object.to_date.strftime("%Y-%m-%d")
+            last_date = self.object.to_date
+            one_hour_later = last_date + timedelta(hours=1)
+            start_date = last_date.strftime("%Y-%m-%d")
+            to_date = one_hour_later.strftime("%Y-%m-%d")
+            start_time = last_date.strftime("%H:%M")
+            end_time = one_hour_later.strftime("%H:%M")
             url = group_aware_reverse('cosinnus:event:conference-event-add',
                                       kwargs={'room_slug': self.room.slug,
                                               'group': self.group})
-            if to_date:
-                url = '{}?start={}&end={}'.format(url, to_date, to_date)
+            if start_date:
+                url = '{}?start={}&end={}&starttime={}&endtime={}'.format(
+                    url,
+                    start_date,
+                    to_date,
+                    start_time,
+                    end_time
+                )
         else:
             # redirect to room, except in compact mode where we redirect to the conference event list
             if settings.COSINNUS_CONFERENCES_USE_COMPACT_MODE:
