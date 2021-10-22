6b6fcb47cd8c331a7ddc4219cf366ef7b037899c
RashidRysaev <rashid.rysaev93@gmail.com>
2021-10-22 09:33:24 +0200

    Code cleaned up; video conference type field moved under the description textbox

--- cosinnus_event/models.py
+++ cosinnus_event/models.py
@@ -719,8 +719,6 @@ class ConferenceEvent(Event):
     stream_key = models.CharField(_('Stream Key'), 
         help_text=_('The key for this specific stream session you created at your streaming provider'),
         blank=True, max_length=250, null=True)
-    
-    # conference_settings_assignments = GenericRelation('cosinnus.CosinnusConferenceSettings')
 
     class Meta(BaseTaggableObjectModel.Meta):
         ordering = ['from_date', 'to_date', 'title']
@@ -748,129 +746,15 @@ class ConferenceEvent(Event):
         # create a "going" attendance for the event's creator
         if settings.COSINNUS_EVENT_MARK_CREATOR_AS_GOING and created and self.state == ConferenceEvent.STATE_SCHEDULED:
             EventAttendance.objects.get_or_create(event=self, user=self.creator, defaults={'state':EventAttendance.ATTENDANCE_GOING})
-        
-        # if self.can_have_bbb_room():
-        #     # we do not create a bbb room on the server yet, that only happens
-        #     # once  `get_bbb_room_url()` is called
-        #     try:
-        #         self.sync_bbb_members()
-        #     except Exception as e:
-        #         logger.exception(e)
-                
-        # # save changed properties of the BBBRoom
-        # self.check_and_sync_bbb_room()
-        # # trigger any streamer status changes if enabled, so streamers
-        # # are started/stopped instantly on changes
-        # trigger_streamer_status_changes(events=[self])
-        
     
     def can_have_bbb_room(self):
         """ Check if this event may have a BBB room """
-        return self.type in self.BBB_ROOM_TYPES and not self.is_break
-    
-    # def check_and_create_bbb_room(self, threaded=True):
-    #     """ Can be safely called at any time to create a BBB room for this event
-    #         if it doesn't have one yet.
-    #         @return True if a room needed to be created, False if none was created """
-    #     # if event is of the right type and has no BBB room yet,
-    #     if self.can_have_bbb_room() and not self.media_tag.bbb_room:
-    #         # be absolutely sure that no room has been created right now
-    #         self.media_tag.refresh_from_db()
-    #         if self.media_tag.bbb_room:
-    #             return False
-            
-    #         # start a thread and create a BBB Room
-    #         event = self
-    #         portal = CosinnusPortal.get_current()
-            
-    #         def create_room():
-    #             max_participants = None
-    #             if event.type in event.BBB_MAX_PARTICIPANT_TYPES and event.max_participants:
-    #                 max_participants = event.max_participants
-    #             # determine BBBRoom type from event type
-    #             room_type = event.BBB_ROOM_ROOM_TYPE_MAP.get(event.type, settings.BBB_ROOM_TYPE_DEFAULT)
-    #             presentation_url = event.presentation_file.url if event.presentation_file else None
-                    
-    #             from cosinnus.models.bbb_room import BBBRoom
-    #             bbb_room = BBBRoom.create(
-    #                 name=event.title,
-    #                 meeting_id=f'{portal.slug}-{event.group.id}-{event.id}',
-    #                 max_participants=max_participants,
-    #                 room_type=room_type,
-    #                 presentation_url=presentation_url,
-    #                 source_object=self,
-    #             )
-    #             event.media_tag.bbb_room = bbb_room
-    #             event.media_tag.save()
-    #             # sync all bb users
-    #             event.sync_bbb_members()
-            
-    #         if threaded:
-    #             class CreateBBBRoomThread(Thread):
-    #                 def run(self):
-    #                     create_room()
-    #             CreateBBBRoomThread().start()
-    #         else:
-    #             create_room()
-    #         return True
-    #     return False
-    
-    # def check_and_sync_bbb_room(self):
-    #     """ Will check if there is a BBBRoom attached to this event,
-    #         and if so, sync the settings like participants from this event with it """
-    #     if self.media_tag.bbb_room:
-    #         bbb_room = self.media_tag.bbb_room
-    #         max_participants = None
-    #         if self.type in self.BBB_MAX_PARTICIPANT_TYPES and self.max_participants:
-    #             max_participants = self.max_participants
-    #         # monkeypatch for BBB appearently allowing one less persons to enter a room
-    #         if max_participants is not None and settings.BBB_ROOM_FIX_PARTICIPANT_COUNT_PLUS_ONE:
-    #             max_participants += 1
-    #         # determine BBBRoom type from event type
-    #         room_type = self.BBB_ROOM_ROOM_TYPE_MAP.get(self.type, settings.BBB_ROOM_TYPE_DEFAULT)
-    #         presentation_url = self.presentation_file.url if self.presentation_file else None
-    #         bbb_room.name = self.title
-    #         bbb_room.max_participants = max_participants
-    #         bbb_room.room_type = room_type
-    #         bbb_room.presentation_url = presentation_url
-    #         bbb_room.save()
-    
-    # def sync_bbb_members(self):
-    #     """ Completely re-syncs all users for this room """
-    #     if self.media_tag.bbb_room:
-    #         bbb_room = self.media_tag.bbb_room
-    #         with transaction.atomic():
-    #             bbb_room.remove_all_users()
-    #             bbb_room.join_group_members(self.group)
-    #             # creator and presenters are moderators in addition to the group admins
-    #             bbb_room.join_user(self.creator, as_moderator=True)
-    #             for user in self.presenters.all():
-    #                 bbb_room.join_user(user, as_moderator=True)
-                    
+        return self.type in self.BBB_ROOM_TYPES and not self.is_break    
     
     def get_absolute_url(self):
         if settings.COSINNUS_CONFERENCES_USE_COMPACT_MODE:
             return super(ConferenceEvent, self).get_absolute_url()
         return group_aware_reverse('cosinnus:conference:room-event', kwargs={'group': self.group, 'slug': self.room.slug, 'event_id': self.id}).replace('%23/', '#/')
-    
-    # def get_bbb_room_url(self):
-    #     if not self.can_have_bbb_room():
-    #         return None
-    #     if self.can_have_bbb_room() and not self.media_tag.bbb_room:
-    #         self.check_and_create_bbb_room(threaded=True)
-    #         # redirect to a temporary URL that refreshes
-    #         return reverse('cosinnus:bbb-room-queue', kwargs={'mt_id': self.media_tag.id})
-    #     return self.media_tag.bbb_room.get_absolute_url()
-    
-    # def get_bbb_room_queue_api_url(self):
-    #     if not self.can_have_bbb_room():
-    #         return None
-    #     if not settings.COSINNUS_TRIGGER_BBB_ROOM_CREATION_IN_QUEUE:
-    #         # create a room here if mode is on hesitant-creation
-    #         if self.can_have_bbb_room() and not self.media_tag.bbb_room:
-    #             self.check_and_create_bbb_room(threaded=True)
-    #         # redirect to a temporary URL that refreshes
-    #     return reverse('cosinnus:bbb-room-queue-api', kwargs={'mt_id': self.media_tag.id})
     
     def get_edit_url(self):
         return group_aware_reverse('cosinnus:event:conference-event-edit', kwargs={'group': self.group, 'room_slug': self.room.slug, 'slug': self.slug})
@@ -923,4 +807,3 @@ def annotate_attendants_count(qs):
                 )
             )
         )
-    
