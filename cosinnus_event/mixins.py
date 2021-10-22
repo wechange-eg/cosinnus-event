b4cd21c7f2cae4006fea6e283da1ab63fcb5dedb
RashidRysaev <rashid.rysaev93@gmail.com>
2021-10-22 11:48:51 +0200

    Mixin cleaned up: not existing fields for Event model taken out

--- cosinnus_event/mixins.py
+++ cosinnus_event/mixins.py
@@ -60,20 +60,10 @@ class BBBRoomMixin(object):
             portal = CosinnusPortal.get_current()
             
             def create_room():
-                max_participants = None
-                if event.type in event.BBB_MAX_PARTICIPANT_TYPES and event.max_participants:
-                    max_participants = event.max_participants
-                # determine BBBRoom type from event type
-                room_type = event.BBB_ROOM_ROOM_TYPE_MAP.get(event.type, settings.BBB_ROOM_TYPE_DEFAULT)
-                presentation_url = event.presentation_file.url if event.presentation_file else None
-                    
                 from cosinnus.models.bbb_room import BBBRoom
                 bbb_room = BBBRoom.create(
                     name=event.title,
                     meeting_id=f'{portal.slug}-{event.group.id}-{event.id}',
-                    max_participants=max_participants,
-                    room_type=room_type,
-                    presentation_url=presentation_url,
                     source_object=self,
                 )
                 event.media_tag.bbb_room = bbb_room
@@ -96,19 +86,7 @@ class BBBRoomMixin(object):
             and if so, sync the settings like participants from this event with it """
         if self.media_tag.bbb_room:
             bbb_room = self.media_tag.bbb_room
-            max_participants = None
-            if self.type in self.BBB_MAX_PARTICIPANT_TYPES and self.max_participants:
-                max_participants = self.max_participants
-            # monkeypatch for BBB appearently allowing one less persons to enter a room
-            if max_participants is not None and settings.BBB_ROOM_FIX_PARTICIPANT_COUNT_PLUS_ONE:
-                max_participants += 1
-            # determine BBBRoom type from event type
-            room_type = self.BBB_ROOM_ROOM_TYPE_MAP.get(self.type, settings.BBB_ROOM_TYPE_DEFAULT)
-            presentation_url = self.presentation_file.url if self.presentation_file else None
             bbb_room.name = self.title
-            bbb_room.max_participants = max_participants
-            bbb_room.room_type = room_type
-            bbb_room.presentation_url = presentation_url
             bbb_room.save()
     
     def sync_bbb_members(self):
@@ -118,10 +96,6 @@ class BBBRoomMixin(object):
             with transaction.atomic():
                 bbb_room.remove_all_users()
                 bbb_room.join_group_members(self.group)
-                # creator and presenters are moderators in addition to the group admins
-                bbb_room.join_user(self.creator, as_moderator=True)
-                for user in self.presenters.all():
-                    bbb_room.join_user(user, as_moderator=True)
 
     def get_bbb_room_url(self):
         if not self.can_have_bbb_room():
