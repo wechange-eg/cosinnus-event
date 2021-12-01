0832cc7f045bb287be576091b30214847823b1ea
Sascha Narr <saschanarr@gmail.com>
2021-12-01 11:57:24 +0100

    Fix cronjob error for portals without conferences

--- cosinnus_event/cron.py
+++ cosinnus_event/cron.py
@@ -5,16 +5,14 @@ from cosinnus.cron import CosinnusCronJobBase
 from cosinnus_event.utils.bbb_streaming import trigger_streamer_status_changes
 
 
-if settings.COSINNUS_CONFERENCES_ENABLED and settings.COSINNUS_CONFERENCES_STREAMING_ENABLED:
+class TriggerBBBStreamers(CosinnusCronJobBase):
+    """ Triggers creates/starts/stops/deletes for streamers for ConferenceEvents. """
     
-    class TriggerBBBStreamers(CosinnusCronJobBase):
-        """ Triggers creates/starts/stops/deletes for streamers for ConferenceEvents. """
-        
-        RUN_EVERY_MINS = 1 # every 1 min (or every time the cron runs at least)
-        schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-        
-        cosinnus_code = 'cosinnus.trigger_bbb_streamers'
-        
-        def do(self):
-            if settings.COSINNUS_CONFERENCES_ENABLED and settings.COSINNUS_CONFERENCES_STREAMING_ENABLED:
-                trigger_streamer_status_changes()
+    RUN_EVERY_MINS = 1 # every 1 min (or every time the cron runs at least)
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    
+    cosinnus_code = 'cosinnus.trigger_bbb_streamers'
+    
+    def do(self):
+        if settings.COSINNUS_CONFERENCES_ENABLED and settings.COSINNUS_CONFERENCES_STREAMING_ENABLED:
+            trigger_streamer_status_changes()
